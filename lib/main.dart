992ab1f435b993b6edf043511393828041e0acb5
shunm-999 <shun-miyazaki@cybozu.co.jp>
2023-02-24 17:04:19 +0900

    device previewの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  return runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) {
+      return const MyApp();
+    },
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,9 +18,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       title: "Flutter Material Design",
       home: Container(
-        color: Colors.black,
+        color: Colors.white,
         child: Center(
             child: Container(
           color: Colors.white,
